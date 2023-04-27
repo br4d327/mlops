fa8a7cf89a9f1156693955f7ac9973734958021d
Basserti <51204419+Basserti@users.noreply.github.com>
2023-04-28 04:02:01 +0500

    Update task7.py

--- scripts_lab4/task7.py
+++ scripts_lab4/task7.py
@@ -5,9 +5,9 @@ train = pd.read_csv('data/train.csv', index_col=0)
 test = pd.read_csv('data/test.csv', index_col=0)
 
 
-df_train = train[['Pclass', 'Sex', 'Age']]
-df_test = test[['Pclass', 'Sex', 'Age']]
+train = train[['Pclass', 'Sex', 'Age']]
+test  = test[['Pclass', 'Sex', 'Age']]
 
 
-df_train.to_csv('data/train(PcSexAge).csv')
-df_test.to_csv('data/test(PcSexAge).csv')
+train.to_csv('data/train(PcSexAge).csv')
+test.to_csv('data/test(PcSexAge).csv')
